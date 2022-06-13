ffbe93b130b1541de3aabda603182d3dc0594140
anatolii <batkovich327@gmail.com>
2022-06-13 15:30:59 +0300

    Update User model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,75 +1,72 @@
-from django.contrib.auth.hashers import make_password, identify_hasher
-from django.db.models import EmailField, BooleanField
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.db import models
 
 
-class UserManager(BaseUserManager):
-    use_in_migrations = True
-
-    def create_user(self, email, password=None, is_active=True, is_staff=None, is_admin=None):
-
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
         if not email:
-            raise ValueError("The given email nust be set")
+            raise ValueError('Users must have an email address')
 
-        if not password:
-            raise ValueError("The given password nust be set")
+        user = self.model(
+            email=self.normalize_email(email)
+        )
 
-        email = self.normalize_email(email)
-        user = self.model(email=email)
         user.set_password(password)
-        user.staff = is_staff
-        user.admin = is_admin
-        user.is_active = is_active
         user.save(using=self._db)
-
         return user
 
     def create_superuser(self, email, password=None):
-        user = self.create_user(email, password=password, is_staff=True, is_admin=True)
-
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password
+        )
+        user.is_admin = True
+        user.save(using=self._db)
         return user
 
-    def create_staffuser(self, email, password=None):
-        user = self.create_user(email, password=password, is_staff=True, is_admin=False)
 
-        return user
+class MyUser(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    city = models.ForeignKey('scraping.City', on_delete=models.SET_NULL,
+                             null=True, blank=True)
+    program_language = models.ForeignKey('scraping.ProgramLanguage', on_delete=models.SET_NULL,
+                                 null=True, blank=True)
+    send_email = models.BooleanField(default=True)
 
-
-class User(AbstractBaseUser):
-    email = EmailField(unique=True, max_length=255)
-    staff = BooleanField(default=False)
-    is_active = BooleanField(default=True)
-    admin = BooleanField(default=False)
+    objects = MyUserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
-
-    object = UserManager()
 
     def __str__(self):
         return self.email
 
     def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
         return True
 
     def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
         return True
 
     @property
     def is_staff(self):
-        if self.admin:
-            return True
-
-        return self.staff
-
-    @property
-    def is_admin(self):
-        return self.admin
-
-    def save(self, *args, **kwargs):
-        try:
-            _alg = identify_hasher(self.password)
-        except ValueError:
-            self.password = make_password(self.password)
-
-        super().save(*args, **kwargs)
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
